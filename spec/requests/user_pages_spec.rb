7fa67bfb40b01b300e6e387a78abde1b12493c5a
Mikkel Klitte <mklitte@4630-mklitte.local>
2014-06-23 15:17:30 +0200

    Chapter 7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,18 +21,39 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      describe "after submit" do
+      	before { click_button submit }
+      	it { should have_title(full_title('Sign up')) }
+      	it { should have_content("error") }
+      end
     end
 
     describe "with valid information" do
+      let(:full_name) { "Example User" }	
+      let(:email) { "user@example.com" }
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
+        fill_in "Name",         with: full_name
+        fill_in "Email",        with: email
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "After submit" do
+      	before { click_button submit }
+        it { should have_title(full_title(full_name)) }
+        it { should have_content('Welcome to the Sample App!') }
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: email) }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
